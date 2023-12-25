da0c0fac2f189bee8543deeb8cc703f019c2e00a
kagepedia <k4gepedia@gmail.com>
2023-12-25 11:59:49 +0900

    client側の実装を追加

---
+++ cmd/client/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	hellopb "grpc-sample-app/pb/greet"
+	"log"
+	"os"
+	"strconv"
+)
+
+var (
+	scanner *bufio.Scanner
+	client  hellopb.GreetingServiceClient
+)
+
+func main() {
+	fmt.Println("start gRPC Client.")
+
+	// 1. 標準入力から文字列を受け取るスキャナを用意
+	scanner = bufio.NewScanner(os.Stdin)
+
+	// 2. gRPCサーバーとのコネクションを確立
+	address := "localhost:8080"
+	conn, err := grpc.Dial(
+		address,
+
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		log.Fatal("Connection failed.")
+		return
+	}
+	defer conn.Close()
+
+	// 3. gRPCクライアントを生成
+	client = hellopb.NewGreetingServiceClient(conn)
+
+	for {
+		fmt.Println("1: send Request")
+		fmt.Println("2: exit")
+		fmt.Print("please enter >")
+
+		scanner.Scan()
+		in := scanner.Text()
+
+		switch in {
+		case "1":
+			Hello()
+
+		case "2":
+			fmt.Println("bye.")
+			goto M
+		}
+	}
+M:
+}
+
+func Hello() {
+	fmt.Println("Please enter your name.")
+	scanner.Scan()
+	name := scanner.Text()
+
+	fmt.Println("Please enter your age.")
+	scanner.Scan()
+	age, e := strconv.Atoi(scanner.Text())
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
+
+	req := &hellopb.HelloRequest{
+		Name: name,
+		Age:  int32(age),
+	}
+	res, err := client.Hello(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res.GetMessage())
+	}
+}
